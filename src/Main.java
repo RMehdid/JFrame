f3a7096c1bf5ab3d2e7412f6209f0f5163a42f71
RMehdid <s.mahdid@univ-boumerdes.dz>
2023-11-10 23:23:48 +0100

    added first custom jFrame

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
+import javax.swing.*;
+import java.awt.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        MyFrame myFrame = new MyFrame();
     }
 }
