2bef261f583ebc67af3d7afda155e4a1e221f9dd
RMehdid <s.mahdid@univ-boumerdes.dz>
2023-11-10 23:43:07 +0100

    added labels

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,31 @@
 import javax.swing.*;
+import javax.swing.border.Border;
 import java.awt.*;
 
 public class Main {
     public static void main(String[] args) {
         MyFrame myFrame = new MyFrame();
+
+        ImageIcon image = new ImageIcon("logo.png");
+        Border border = BorderFactory.createLineBorder(Color.green, 3);
+
+        JLabel label = new JLabel();
+        label.setText("Bro, do you even code?");
+        label.setForeground(Color.green);
+        label.setFont(new Font("MV Boli",Font.PLAIN, 50));
+        label.setIcon(image);
+        label.setHorizontalTextPosition(JLabel.CENTER);
+        label.setVerticalTextPosition(JLabel.TOP);
+        label.setIconTextGap(20);
+        label.setBackground(Color.black);
+        label.setOpaque(true);
+        label.setBorder(border);
+        label.setVerticalAlignment(JLabel.CENTER);
+        label.setHorizontalAlignment(JLabel.CENTER);
+//        label.setBounds(100, 100, 250, 250);
+
+        myFrame.add(label);
+//        myFrame.setLayout(null);
+        myFrame.pack();
     }
 }
--- src/MyFrame.java
+++ src/MyFrame.java
@@ -1,17 +1,13 @@
 import javax.swing.*;
-import java.awt.*;
 
 public class MyFrame extends JFrame {
     MyFrame() {
         this.setTitle("Samy's frame");
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.setResizable(false);
         this.setSize(420, 420);
         this.setVisible(true);
 
         ImageIcon image = new ImageIcon("logo.png");
         this.setIconImage(image.getImage());
-
-        this.getContentPane().setBackground(new Color(123, 50, 250, 255));
     }
 }
