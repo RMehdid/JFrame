35fbc395532b6641cec5224f25f509fea343dfbf
Samy Abderraouf Mehdid <sr.mehdid@gmail.com>
2023-11-11 18:13:41 +0100

    Model from Serie 1

--- src/Main.java
+++ src/Main.java
@@ -5,29 +5,27 @@ import java.awt.*;
 public class Main {
     public static void main(String[] args) {
 
-        MyFrame myFrame = new MyFrame();
+        JFrame frame = new JFrame("ExempleSimple");
 
-        JPanel redPanel = new JPanel();
-        redPanel.setBackground(Color.red);
-        redPanel.setBounds(0, 0, 100, 100);
-        redPanel.setLayout(null);
-        myFrame.add(redPanel);
+        // Création du container
+        JPanel panel = new JPanel();
 
-        JPanel bluePanel = new JPanel();
-        bluePanel.setBackground(Color.blue);
-        bluePanel.setBounds(100, 0, 100, 100);
-        bluePanel.setLayout(null);
-        myFrame.add(bluePanel);
+        // Définition du gestionnaire de placement
+        panel.setLayout(new GridLayout(1,2));
 
-        JPanel greenPanel = new JPanel();
-        greenPanel.setBackground(Color.green);
-        greenPanel.setBounds(0, 100, 200, 100);
-        greenPanel.setLayout(null);
-        myFrame.add(greenPanel);
+        // Création des composants
+        JLabel label = new JLabel("Entrer votre nom");
+        JTextField textField = new JTextField("toto");
 
-//        ImageIcon image = new ImageIcon("logo.png");
-//        Border border = BorderFactory.createLineBorder(Color.green, 3);
-//        myFrame.add(new MyLabel(image, border));
-//        myFrame.pack();
+        // Ajout des composants au container
+        panel.add(label);
+        panel.add(textField);
+
+        // Ajout du container à la fenêtre
+        frame.getContentPane().add(panel);
+
+        // Afficher la fenêtre
+        frame.pack();
+        frame.setVisible(true);
     }
 }
