29dab51a55dc2f7d6847fbde80b081c64bcedc4c
Samy Abderraouf Mehdid <sr.mehdid@gmail.com>
2023-11-11 18:08:14 +0100

    added colored pannels

--- src/Main.java
+++ src/Main.java
@@ -4,12 +4,30 @@ import java.awt.*;
 
 public class Main {
     public static void main(String[] args) {
+
         MyFrame myFrame = new MyFrame();
 
-        ImageIcon image = new ImageIcon("logo.png");
-        Border border = BorderFactory.createLineBorder(Color.green, 3);
+        JPanel redPanel = new JPanel();
+        redPanel.setBackground(Color.red);
+        redPanel.setBounds(0, 0, 100, 100);
+        redPanel.setLayout(null);
+        myFrame.add(redPanel);
 
-        myFrame.add(new MyLabel(image, border));
-        myFrame.pack();
+        JPanel bluePanel = new JPanel();
+        bluePanel.setBackground(Color.blue);
+        bluePanel.setBounds(100, 0, 100, 100);
+        bluePanel.setLayout(null);
+        myFrame.add(bluePanel);
+
+        JPanel greenPanel = new JPanel();
+        greenPanel.setBackground(Color.green);
+        greenPanel.setBounds(0, 100, 200, 100);
+        greenPanel.setLayout(null);
+        myFrame.add(greenPanel);
+
+//        ImageIcon image = new ImageIcon("logo.png");
+//        Border border = BorderFactory.createLineBorder(Color.green, 3);
+//        myFrame.add(new MyLabel(image, border));
+//        myFrame.pack();
     }
 }
--- src/MyFrame.java
+++ src/MyFrame.java
@@ -4,10 +4,11 @@ public class MyFrame extends JFrame {
     MyFrame() {
         this.setTitle("Samy's frame");
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setLayout(null);
         this.setSize(420, 420);
         this.setVisible(true);
 
-        ImageIcon image = new ImageIcon("logo.png");
-        this.setIconImage(image.getImage());
+//        ImageIcon image = new ImageIcon("logo.png");
+//        this.setIconImage(image.getImage());
     }
 }
